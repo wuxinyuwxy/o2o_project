0b9bf82b025edcb829580209b1e416b4a5155072
wxy <1514905989@qq.com>
2019-11-14 20:43:12 +0800

    解决跨域问题以及提交网关

--- product_service/src/main/java/com/wxy/controller/BuyerProductController.java
+++ product_service/src/main/java/com/wxy/controller/BuyerProductController.java
@@ -5,6 +5,7 @@ import com.wxy.vo.BuyeProductInfoVO;
 import com.wxy.vo.CodeMsg;
 import com.wxy.vo.ResponseVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class BuyerProductController {
     @Autowired
     private ProductService productService;
 
+    @CrossOrigin
     @GetMapping("list")
     public ResponseVO<List<BuyeProductInfoVO>> selectBuyerFoods(){
         //查询数据  类型
